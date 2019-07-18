22521ac7e1907fbefb45ecaf4cd48a749fccc136
KimYoungHoon <gold1201@naver.com>
2019-07-18 17:44:23 +0900

    기초작업(작업자별 스크립트 나누기)

---
+++ 19-1GameProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    MSscript MS;
+    HCscript HC;
+    YHscript YH;
+    JWscript JW;
+	// Use this for initialization
+	void Start () {
+        MS = GetComponent<MSscript>();
+        HC = GetComponent<HCscript>();
+        YH = GetComponent<YHscript>();
+        JW = GetComponent<JWscript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
